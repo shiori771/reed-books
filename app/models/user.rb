09cced0fd389507a6a142b4e0c741bf24c67acfa
shiori771 <16hb218l@gmail.com>
2020-10-27 12:51:38 +0900

    [add] ゲストログイン機能の追加 #5

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  
+  def self.guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.hex(8)
+      user.name = SecureRandom.urlsafe_base64(5)
+    end
+  end
 end
