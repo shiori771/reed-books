78a769d6636a18dc8dc7f416785be76fad2c5307
shiori771 <16hb218l@gmail.com>
2020-10-28 15:04:33 +0900

    [add] book_usersテーブルとアソシエーション作成 #6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_051256) do
+ActiveRecord::Schema.define(version: 2020_10_28_055922) do
+
+  create_table "book_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_users_on_book_id"
+    t.index ["user_id"], name: "index_book_users_on_user_id"
+  end
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image_url", null: false
@@ -35,4 +44,6 @@ ActiveRecord::Schema.define(version: 2020_10_28_051256) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "book_users", "books"
+  add_foreign_key "book_users", "users"
 end
