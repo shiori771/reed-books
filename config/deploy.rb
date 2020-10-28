51b4b0c5c7709a4307ca8314a9574b0247fbe57c
shiori771 <16hb218l@gmail.com>
2020-10-28 09:57:55 +0900

    [fix] リポジトリURL変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "https://github.com/shiori771/reed-books.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
