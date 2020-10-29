116651a6b1f8f310fc4465e0cb03af067b7917f8
shiori771 <16hb218l@gmail.com>
2020-10-29 12:10:58 +0900

    [add] newアクションビューファイルからcreateアクションへ送るパラメータ作成 #6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,19 +21,20 @@ class BooksController < ApplicationController
   # end
 
 
-  # def create
-  #   @book = Book.new(book_params)
-  #   if @book.save
-  #     redirect_to root_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    binding.pry
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
-  # def book_params
-  #   params.permit(:isbn, :medium_image_url, :title, :author, :url).merge(user_id: current_user.id)
-  # end
+  def book_params
+    params.permit(:image_url, :title, :author, :url, :user_id)
+  end
 
 end
