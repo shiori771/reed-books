58320e1dfc67b4c9dee492071e8cc85fefa7573c
shiori771 <16hb218l@gmail.com>
2020-11-03 21:58:04 +0900

    user_idカラム追加

--- db/migrate/20201103101944_add_references_to_books.rb
+++ db/migrate/20201103101944_add_references_to_books.rb
@@ -1,5 +1,5 @@
 class AddReferencesToBooks < ActiveRecord::Migration[6.0]
   def change
-    
+    add_reference :books, :user, null: false, foreign_key: { to_table: :users }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_100257) do
+ActiveRecord::Schema.define(version: 2020_11_03_101944) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image_url", null: false
